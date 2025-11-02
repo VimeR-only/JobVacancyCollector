3b61d5b79c8325c5a1df0a1c6ac3a91a9999241d
-=VimeR=- <85843021+VimeR-only@users.noreply.github.com>
2025-11-02 19:50:26 +0300

    Register new services and update parser usage

--- JobVacancyCollector/Program.cs
+++ JobVacancyCollector/Program.cs
@@ -1,9 +1,10 @@
-﻿
-using JobVacancyCollector.Application;
-using JobVacancyCollector.Application.Abstractions.Repositories;
+﻿using JobVacancyCollector.Application.Abstractions.Repositories;
 using JobVacancyCollector.Application.Abstractions.Scrapers;
+using JobVacancyCollector.Application.Interfaces;
+using JobVacancyCollector.Application.Services;
 using JobVacancyCollector.Infrastructure.Data;
 using JobVacancyCollector.Infrastructure.Parsers.WorkUa;
+using JobVacancyCollector.Infrastructure.Parsers.WorkUa.Html;
 using JobVacancyCollector.Infrastructure.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,8 +17,10 @@ namespace JobVacancyCollector
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddScoped<HtmlWorkUaParser>();
             builder.Services.AddScoped<IVacancyScraper, WorkUaParser>();
             builder.Services.AddScoped<IVacancyRepository, VacancyRepository>();
+            builder.Services.AddScoped<IVacancyService, VacancyService>();
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -39,15 +42,19 @@ namespace JobVacancyCollector
                     //await dbContext.Database.MigrateAsync();
                     //Console.WriteLine("Database migrations applied successfully.");
 
-                    var scraper = services.GetRequiredService<IVacancyScraper>();
+                    //var scraper = services.GetRequiredService<IVacancyScraper>();
 
                     //var vacancies = await scraper.ScrapeAsync("Хмельницький", 1);
-                    await scraper.ScrapeNewVacanciesAsync("Хмельницький", 1);
+                    //await scraper.ScrapeNewVacanciesAsync("Хмельницький", 1);
 
                     //var vacancyRepository = services.GetRequiredService<IVacancyRepository>();
 
                     //await vacancyRepository.ClearAsync();
                     //await vacancyRepository.AddRangeAsync(vacancies);
+
+                    //var vacancies2 = await scraper.ScrapeNotExistentVacanciesAsync("Хмельницький", 1);
+
+                    //Console.WriteLine(vacancies2.Count);
                 }
                 catch (Exception ex)
                 {
